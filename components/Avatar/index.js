e50198460dcca2032c968c77e7472ae3cadf26a0
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-07 14:41:38 -0300

    feat: avatar component
    create Avatar component and includes styles.module.css

---
+++ components/Avatar/index.js
@@ -0,0 +1,13 @@
+import styles from './styles.module.css'
+
+export default function Avatar ({ alt, src, text, withText }) {
+    return (
+        <>
+            <div className={styles.container}>
+                <img className={styles.avatar} alt={alt} src={src} title={alt} />
+                { withText && <strong>{text || alt}</strong>}
+            </div>
+            
+        </>
+    )
+}
