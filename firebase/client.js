ed385c4a6f0eb6ec57558844e73a7e9c09faa95e
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-07 20:11:14 -0300

    perf: firebase client
    add connection to db firestore
    add new function to create dev-tweets and save them in db
    add new function to read dev-tweets from db

--- firebase/client.js
+++ firebase/client.js
@@ -12,12 +12,15 @@ const firebaseConfig = {
 
 !firebase.apps.length && firebase.initializeApp(firebaseConfig)
 
+const db = firebase.firestore()
+
 const mapUserFromFirebaseAuthToUser = (user) => {
-    const {displayName, email, photoURL} = user
+    const {displayName, email, photoURL, uid} = user
     return {
         avatar: photoURL,
         username: displayName,
-        email
+        email,
+        uid
     }
 }
 
@@ -31,4 +34,36 @@ export const onAuthStateChanged = (onChange) => {
 export const loginWithGitHub = () => {
     const githubProvider = new firebase.auth.GithubAuthProvider()
     return firebase.auth().signInWithPopup(githubProvider)
+}
+
+export const addDevit = ({avatar, content, userId, userName}) => {
+    return db.collection('dev-tweets').add({
+        avatar,
+        content,
+        userId,
+        userName,
+        createdAt: firebase.firestore.Timestamp.fromDate(new Date()),
+        likesCount: 0,
+        sharedCount: 0
+    })
+}
+
+export const fetchLatestDevits = () => {
+    return db.collection('dev-tweets')
+        .get()
+        .then(snapshot => {
+            return snapshot.docs.map(doc => {
+                const data = doc.data()
+                const id = doc.id
+                const {createdAt} = data
+                const date = new Date(createdAt.seconds * 1000)
+                const normalizedCreatedAt = new Intl.DateTimeFormat("es-AR").format(date)
+
+                return {
+                    ...data,
+                    id,
+                    createdAt: normalizedCreatedAt
+                }
+            })
+        })
 }
