8325569e18a35707629d1523cc6b5c9381176da5
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-08 16:22:46 -0300

    perf: add icons on homepage
    add icons on footer nav at homepage

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
+import Head from "next/head";
+import Link from 'next/link'
 import AppLayout from "components/AppLayout";
 import Devit from "components/Devit";
 import useUser from "hooks/useUser";
 import { fetchLatestDevits } from "firebase/client";
+import CreateIcon from "components/Icons/Create";
+import HomeIcon from "components/Icons/HomeIcon";
+import Search from "components/Icons/Search";
+import { colors } from "styles/theme";
 
 export default function HomePage () {
     const [timeline, setTimeline] = useState([])
@@ -15,11 +21,14 @@ export default function HomePage () {
     return (
         <>
             <AppLayout>
+                <Head>
+                    <title>Dev-twitter | Inicio</title>
+                </Head>
                 <header>
                     <h2>Inicio</h2>
                 </header>
                 <section>
-                    {timeline.map(({ id, userName, avatar, content, userId, createdAt }) => {
+                    {timeline.map(({ id, userName, avatar, content, img, userId, createdAt }) => {
                         return (
                             <Devit 
                                 key={id}
@@ -28,13 +37,28 @@ export default function HomePage () {
                                 content={content}
                                 createdAt={createdAt}
                                 id={id}
+                                img={img}
                                 userId={userId}
                             />
                         )
                     })}
                 </section>
                 <nav>
-
+                    <Link href="/home">
+                        <a>
+                            <HomeIcon stroke="#09f" width={32} height={32}/>
+                        </a>
+                    </Link>
+                    <Link href="/search">
+                        <a>
+                            <Search stroke="#09f" width={32} height={32}/>
+                        </a>
+                    </Link>
+                    <Link href="/compose/tweet">
+                        <a>
+                            <CreateIcon stroke="#09f" width={32} height={32}/>
+                        </a>
+                    </Link>
                 </nav>
             </AppLayout>
             <style jsx>{`
@@ -51,6 +75,10 @@ export default function HomePage () {
                     width: 100%;
                 }
 
+                section {
+                    flex: 1;
+                }
+
                 h2 {
                     font-size: 21px;
                     font-weight: 800;
@@ -61,11 +89,29 @@ export default function HomePage () {
                     background: #ffffff;
                     border-top: 1px solid #eee;
                     bottom: 0;
+                    display: flex;
                     height: 49px;
                     position: sticky;
                     width: 100%;
                 }
 
+                nav a {
+                    align-items: center;
+                    display: flex;
+                    flex: 1 1 auto;
+                    height: 100%;
+                    justify-content: center;
+                }
+
+                nav a:hover {
+                    background: radial-gradient(#0099ff44 15%, transparent 15%);
+                    background-size: 180px 180px;
+                    background-position: center;
+                }
+
+                nav a:hover > :global(svg) {
+                    stroke: ${colors.primary}
+                }
             `}</style>
         </>
     )
