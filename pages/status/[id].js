85630117c8a121830b2c119829e666fcfc9b6bb5
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-09 18:57:58 -0300

    feat: status page with id data
    create status page to set tweets by id
    includes getStaticPaths and getStaticProps to fetch data
    includes examples with getServerSideProps and getInitialProps

---
+++ pages/status/[id].js
@@ -0,0 +1,170 @@
+import Devit from "components/Devit";
+import CreateIcon from "components/Icons/Create";
+import HomeIcon from "components/Icons/HomeIcon";
+import Search from "components/Icons/Search";
+import { firestore } from "firebase/admin";
+import { useRouter } from "next/router";
+import Head from "next/head";
+import Link from 'next/link'
+import { colors } from "styles/theme";
+import Loading from "components/Loading";
+
+
+export default function DevitPage(props) {
+    const router = useRouter()
+
+    if (router.isFallback){
+        return <Loading />
+    } 
+
+    return (
+        <>
+            <Head>
+                <title>Dev-tweet | {props.userName}</title>
+            </Head>
+            <header>
+                <h2>Dev-tweet de {props.userName}</h2>
+            </header>
+
+            <section>
+                <Devit {...props}/>
+            </section>
+            
+            <nav>
+                <Link href="/home">
+                    <a>
+                        <HomeIcon stroke="#09f" width={32} height={32}/>
+                    </a>
+                </Link>
+                <Link href="/search">
+                    <a>
+                        <Search stroke="#09f" width={32} height={32}/>
+                    </a>
+                </Link>
+                <Link href="/compose/tweet">
+                    <a>
+                        <CreateIcon stroke="#09f" width={32} height={32}/>
+                    </a>
+                </Link>
+            </nav>
+            <style jsx>{`
+                header {
+                    align-items: center;
+                    background: #ffffffaa;
+                    backdrop-filter: blur(5px);
+                    border-bottom: 1px solid #eee;
+                    display: flex;
+                    height: 49px;
+                    position: sticky;
+                    top: 0;
+                    width: 100%;
+                }
+
+                h2 {
+                    font-size: 21px;
+                    font-weight: 800;
+                    padding-left: 15px;
+                }
+
+                section {
+                    flex: 1;
+                }
+
+                nav {
+                    background: #ffffff;
+                    bottom: 0;
+                    display: flex;
+                    height: 49px;
+                    position: sticky;
+                    width: 100%;
+                }
+
+                nav a {
+                    align-items: center;
+                    display: flex;
+                    flex: 1 1 auto;
+                    height: 100%;
+                    justify-content: center;
+                }
+
+                nav a:hover {
+                    background: radial-gradient(#0099ff44 15%, transparent 15%);
+                    background-size: 180px 180px;
+                    background-position: center;
+                }
+
+                nav a:hover > :global(svg) {
+                    stroke: ${colors.primary}
+                }
+            `}</style>
+        </>
+    )
+}
+
+/**
+ * example getStaticPaths and getStaticProps
+ */
+
+export async function getStaticPaths () {
+    return {
+        paths: [{ params: { id: "Zglkuf4IEWSOvU8IRDLu" } }],
+        fallback: true,
+    }
+}
+
+export async function getStaticProps (context) {
+    const { params } = context
+    const { id } = params
+
+    return firestore.collection('dev-tweets').doc(id).get()
+        .then(doc => {
+            const data = doc.data()
+            const id = doc.id
+            const {createdAt} = data
+                
+            const props = {
+                ...data,
+                id,
+                createdAt: +createdAt.toDate(),
+            }
+            return {props}
+        })
+        .catch(() => {
+            return { props: {} }
+        })
+}
+
+/**
+ * example getServerSideProps
+ */
+
+/*export async function getServerSideProps (context) {
+    const { params, res } = context
+    const { id } = params
+
+    const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+    if (apiResponse.ok) {
+        const props = await apiResponse.json()
+        return {props}
+    }
+    if (res) {
+        res.writeHead(301, { Location: "/home" }).end()
+    }
+}*/
+
+/**
+ * example getInitialProps
+ */
+
+/*DevitPage.getInitialProps = (context) => {
+    const { query, res } = context
+    const { id } = query
+
+    return fetch(`http://localhost:3000/api/devits/${id}`)
+        .then(apiResponse => {
+            if (apiResponse.ok) return apiResponse.json()
+            if (res) {
+                res.writeHead(301, { Location: "/home" }).end()
+            }
+        })
+}*/
