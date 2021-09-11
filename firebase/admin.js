8cd1f0d4a652b5ee70cc1a2f9a44afbe0ab095d0
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-10 21:30:11 -0300

    add vercel into project add enviroment variable to firebase admin

--- firebase/admin.js
+++ firebase/admin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-keys.json");
+const serviceAccount = require(JSON.parse(process.env.FIREBASE_KEYS));
 
 try {
     admin.initializeApp({
