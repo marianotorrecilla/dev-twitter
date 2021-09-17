1d2b418485b51e1526526aef88a24c3717028beb
marianotorrecilla <marianotorrecilla89@hotmail.com>
2021-09-17 10:34:44 -0300

    feat: create OutIcon component
    add new icon component to log out

---
+++ components/Icons/Out.js
@@ -0,0 +1,18 @@
+import * as React from "react"
+
+export default function OutIcon(props) {
+    return (
+        <svg height={21} width={21} xmlns="http://www.w3.org/2000/svg" {...props}>
+        <g
+            fill="none"
+            fillRule="evenodd"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <path d="M11.5 13.535l-3-3.035 3-3M18.5 10.5h-10" />
+            <path d="M16.5 8.5V5.54a2 2 0 00-1.992-2l-8-.032A2 2 0 004.5 5.5v10a2 2 0 002 2h8a2 2 0 002-2v-3" />
+        </g>
+        </svg>
+    )
+}
